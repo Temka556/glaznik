d782b4e01f71da8c7957fb74311ca497d72f4942
Temka556 <99419421+Temka556@users.noreply.github.com>
2023-04-08 11:28:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+token = '6205790876:AAGgkSng-GxENxJHJDcNCi8Pt9EYQ-nUngo' # Токен бота
+loginbot = 'Mosint2_bot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+QIWI_TOKEN = 'xxxxxxxx' # токен киви
+QIWI_NUMBER = '77777777777' # Номер киви без +
+
+admin_id = 5304556451 # Ваш айди
+
+price = 190 # Цена за доступ
+
+#Ссылка на канал - @slivmenss
+#Наш чат - @slivmens_chat
